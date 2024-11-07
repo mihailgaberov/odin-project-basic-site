7b08c006ee15b6beb58f1e5bdda2f05b3ef73f71
mihailgaberov <mihail.gaberov@gmail.com>
2024-11-07 12:58:16 +0100

    chore: fix 404 case

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-// error handler
-app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "/404.html"));
-});
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "/index.html"));
@@ -17,6 +13,10 @@ app.get("/about", function (req, res) {
 
 app.get("/contact", function (req, res) {
   res.sendFile(path.join(__dirname, "/contact-me.html"));
+});
+
+app.get("*", function (req, res) {
+  res.status(404).sendFile(path.join(__dirname, "/404.html"));
 });
 
 const PORT = process.env.PORT || 3000;
